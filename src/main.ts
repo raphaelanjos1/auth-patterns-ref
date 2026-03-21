4283804febe1e5a2c024931124444925cd1bcd55
Raphael <raphaelanjosroa@gmail.com>
2026-03-21 12:26:17 -0300

    feat: add basic Swagger setup

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Auth Patterns Ref')
+    .setDescription('Auth Patterns Reference Implementation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
